ebc900393d0d7c3d508cc937d31476a05dc4f037
Jorge L <jorge.alfmur@gmail.com>
2018-10-19 22:32:31 -0500

    conexión a db localmente

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('./config/config');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -9,6 +10,13 @@ app.use(bodyParser.json());
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(process.env.PORT, () => {
-  console.log(`Servidor corriendo 🚀 en el puerto ${process.env.PORT}!`);
+mongoose.connect('mongodb://localhost/cafe', { useNewUrlParser: true });
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'Error en conexión'));
+db.once('open', () => {
+  console.log('Conexión correcta');
+  app.listen(process.env.PORT, () => {
+    console.log(`Servidor corriendo 🚀 en el puerto ${process.env.PORT}!`);
+  });
 });
