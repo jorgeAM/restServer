8a9147ef2a0fe883ea76307834ecada68cc7083c
Jorge L <jorge.alfmur@gmail.com>
2018-10-19 23:18:26 -0500

    modelo de usuario

---
+++ models/usuario.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const usuarioSchema = new Schema({
+  nombre: {
+    type: String,
+    required: [true, 'El nombre es necesario'],
+  },
+  email: {
+    type: String,
+    required: [true, 'El correo es necesario'],
+    unique: true,
+    trim: true,
+    match: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+  },
+  password: {
+    type: String,
+    required: [true, 'La contraseña es necesaria'],
+    trim: true,
+  },
+  avatar: String,
+  role: {
+    type: String,
+    required: true,
+    enum: ['user', 'admin'],
+    default: 'user',
+  },
+  estado: {
+    type: Boolean,
+    default: true,
+  },
+  google: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const Usuario = mongoose.model('Usuario', usuarioSchema);
+
+module.exports = Usuario;
