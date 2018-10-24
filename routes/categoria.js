eb9dbf2494030aba88a2fae8ab1ed4d99340f312
Jorge L <jorge.alfmur@gmail.com>
2018-10-23 20:58:28 -0500

    CRUD categoria

---
+++ routes/categoria.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const Categoria = require('../models/categoria');
+const isAuthenticate = require('../middlewares/authentication');
+
+const app = express.Router();
+
+app.get('/categorias', isAuthenticate, async (req, res) => {
+  try {
+    const categorias = await Categoria.find().populate('usuario', 'nombre email');
+    res.status(200).json({ categorias });
+  } catch (err) {
+    res.status(500).json({ err });
+  }
+});
+
+app.get('/categoria/:id', isAuthenticate, async (req, res) => {
+  try {
+    const { id } = req.params;
+    const categoria = await Categoria.findById(id).populate('usuario', 'nombre email');
+    if (!categoria) return res.status(404).json({
+      message: `Categoria con id ${id} no encontrado`,
+    });
+    res.status(200).json({ categoria });
+  } catch (err) {
+    res.status(500).json({ err });
+  }
+});
+
+app.post('/categoria', isAuthenticate, async (req, res) => {
+  try {
+    const { body } = req;
+    const nuevaCategoria = new Categoria();
+    nuevaCategoria.descripcion = body.descripcion;
+    nuevaCategoria.usuario = req.user.sub;
+    const categoria = await nuevaCategoria.save();
+    res.status(200).json({ categoria });
+  } catch (err) {
+    res.status(500).json({ err });
+  }
+});
+
+app.put('/categoria/:id', isAuthenticate, async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { body } = req;
+    const categoria = await Categoria.findOneAndUpdate(id, body, { new: true });
+    if (!categoria) return res.status(404).json({
+      message: `Categoria con id ${id} no encontrado`,
+    });
+    res.status(200).json({ categoria });
+  } catch (err) {
+    res.status(500).json({ err });
+  }
+});
+
+app.delete('/categoria/:id', isAuthenticate, async (req, res) => {
+  try {
+    const { id } = req.params;
+    const categoria = await Categoria.findOneAndDelete(id);
+    if (!categoria) return res.status(404).json({
+      message: `Categoria con id ${id} no encontrado`,
+    });
+    res.status(200).json({ categoria });
+  } catch (err) {
+    res.status(500).json({ err });
+  }
+});
+
+module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 /*RUTAS*/
 const userRoutes = require('./usuario');
 const loginRoutes = require('./login');
+const categoryRoutes = require('./categoria');
 
 const app = express();
 
 app.use(userRoutes);
 app.use(loginRoutes);
+app.use(categoryRoutes);
 
 module.exports = app;
