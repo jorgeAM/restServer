0fe6ee3e0ab8be107b5532ee4c6c659515d619e4
Jorge L <jorge.alfmur@gmail.com>
2018-10-27 22:40:59 -0500

    configuracion subida de imagenes prd pt2

--- routes/upload.js
+++ routes/upload.js
@@ -26,7 +26,8 @@ app.post('/upload/:tipo/:id', isAuthenticate, async (req, res) => {
   if (image.mimetype === 'image/png' || image.mimetype === 'image/jpeg') {
     let extension = image.name.split('.')[1];
     let nombreArchivo = `${moment().unix()}.${extension}`;
-    image.mv(`/uploads/${tipo}/${nombreArchivo}`, (err) => {
+    let ruta = path.resolve(__dirname, `../uploads/${tipo}/${fileName}`);
+    image.mv(ruta, (err) => {
       if (err) return res.status(400).json({ err });
       else {
         /*DB*/
